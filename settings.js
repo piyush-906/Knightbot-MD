ac9889b54b5a76b5147c14711a3e217c45334c5c
piyush-906 <hackerpiyushyt@gmail.com>
2025-05-03 23:59:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'LUCIFER OP', // Your name
-  ownerNumber: '918145968445', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '917432077015', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
